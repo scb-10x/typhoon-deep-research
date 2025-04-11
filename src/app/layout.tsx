c40b41f0d0af94af641e43664b3175b43facd3b3
Pittawat Taveekitworachai <11158905+Pittawat2542@users.noreply.github.com>
2025-04-11 10:39:03 +0700

    chore: add GTM

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -108,8 +108,21 @@ export default function RootLayout({
             })
           }}
         />
+        <Script>
+          {`
+      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-WK925XWL');
+    `}
+        </Script>
       </head>
       <body className={inter.className}>
+        <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WK925XWL"
+            height={0} width={0} style={{ display: 'none', visibility: 'hidden' }}></iframe>
+        </noscript>
         <div className="min-h-screen bg-gradient-to-b from-gray-50 to-gray-100 flex flex-col">
           <LanguageProvider>
             <Navigation />
